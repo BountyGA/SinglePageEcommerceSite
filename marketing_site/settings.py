e931bbae27a7340e9701088a820ecf47d78e1b43
BountyGA <gabrielbounty19@gmail.com>
2025-09-07 01:24:56 +0100

    Update settings.py

--- marketing_site/settings.py
+++ marketing_site/settings.py
@@ -17,7 +17,7 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://singlepageecommercesite.onrender.com/"]
 
 
 # Application definition
